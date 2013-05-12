c97d87eaa8f7d0f1746cdf0e92fbeee6ae72722c
Michael Gan <gc1108960@gmail.com>
2013-05-12 16:34:06 +0900

    Create start.php

---
+++ start.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ *  This file is used for Laravel 3 , please ignore it.
+ */
+
+Autoloader::namespaces(array(
+  'Purekid\Mongodm' => Bundle::path('mongodm')."src/Purekid/Mongodm"
+));
