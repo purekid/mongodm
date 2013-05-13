59cb7c6ce8a991b301122251a37bf0af82a41c00
Michael.Gan <gc1108960@gmail.com>
2013-05-13 17:57:18 +0800

    add a test case

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,7 @@ $loader = require_once $file;
 $map = array(
 		'Purekid\Mongodm\Test\Model\User'=> __DIR__."/Models/User.php",
 		'Purekid\Mongodm\Test\Model\Book'=> __DIR__."/Models/Book.php",
-		'Purekid\Mongodm\Test\Model\Student'=> __DIR__."/Models/Student.php"
+		'Purekid\Mongodm\Test\Model\Student'=> __DIR__."/Models/Student.php",
+		'Purekid\Mongodm\Test\Model\Pupil'=> __DIR__."/Models/Pupil.php"
 		);
 $loader->addClassMap($map);
