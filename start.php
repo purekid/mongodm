711729d73509df44764037eb9f018db762d8c1dc
Michael Gan <gc1108960@gmail.com>
2013-07-19 14:00:02 +0800

    Delete start.php

--- start.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- *  This file is used for Laravel 3 , please ignore it.
- */
-
-Autoloader::namespaces(array(
-  'Purekid\Mongodm' => Bundle::path('mongodm')."src/Purekid/Mongodm"
-));
