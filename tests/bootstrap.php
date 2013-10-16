15af7f840114debebc57581535dc6790ebeff3de
Ryan Schumacher <j.r.schumacher@gmail.com>
2013-10-16 14:44:13 -0500

    Move from Models to Model

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,10 +9,10 @@ if (!file_exists($file = __DIR__.'/../../../autoload.php') && !file_exists($file
 $loader = require_once $file;
 
 $map = array(
-		'Purekid\Mongodm\Test\Model\Base'=> __DIR__."/Models/Base.php",
-		'Purekid\Mongodm\Test\Model\User'=> __DIR__."/Models/User.php",
-		'Purekid\Mongodm\Test\Model\Book'=> __DIR__."/Models/Book.php",
-		'Purekid\Mongodm\Test\Model\Student'=> __DIR__."/Models/Student.php",
-		'Purekid\Mongodm\Test\Model\Pupil'=> __DIR__."/Models/Pupil.php"
+		'Purekid\Mongodm\Test\Model\Base'=> __DIR__."/Model/Base.php",
+		'Purekid\Mongodm\Test\Model\User'=> __DIR__."/Model/User.php",
+		'Purekid\Mongodm\Test\Model\Book'=> __DIR__."/Model/Book.php",
+		'Purekid\Mongodm\Test\Model\Student'=> __DIR__."/Model/Student.php",
+		'Purekid\Mongodm\Test\Model\Pupil'=> __DIR__."/Model/Pupil.php"
 		);
 $loader->addClassMap($map);
