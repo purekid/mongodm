f7bd796870ff42f9460c1b5d386c090e3eb7ed02
Michael.Gan <gc1108960@gmail.com>
2013-10-17 15:30:50 +0800

    remove hostname '192.168.1.2' for testing section

--- config/database.php
+++ config/database.php
@@ -39,8 +39,8 @@ return array(
 	),
 	'testing' => array(
 		'connection' => array(
-			'hostnames' => 'localhost',
-			'database'  => 'test_db'
+			'hostnames' => '192.168.1.105',
+			'database'  => 'test_d22b'
 		)
 	),
 	'production' => array(
