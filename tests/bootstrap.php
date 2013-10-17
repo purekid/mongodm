7ee936b72b37ff7c7041b11343cf691d20534fe1
Ryan Schumacher <j.r.schumacher@gmail.com>
2013-10-17 05:24:37 -0500

    Cleaner message on installing composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 
 if (!file_exists($file = __DIR__.'/../../../autoload.php') && !file_exists($file = __DIR__.'/../vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run test suite.');
+    echo "You must install the dev dependencies using:\n";
+    echo "    composer install --dev\n";
+   exit(1);
 }
 
 $loader = require_once $file;
