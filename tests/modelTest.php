233733c7f4a76602d1fe132d6f22e816f50a3b4a
Tim Rodger <tim.rodger@gmail.com>
2014-01-04 19:05:02 +0000

    Adds Model::find() tests

--- tests/modelTest.php
+++ tests/modelTest.php
@@ -180,6 +180,24 @@ class ModelTest extends PhactoryTestCase
 
     }
 
+    public function testFindWithLimit()
+    {
+        $user = User::find($params = array('name'=>'michael'), $sort = array(), $fields = array(), $limit = 5);
+        $this->assertGreaterThan(0, $user->count());
+    }
+
+    public function testFindWithSkip()
+    {
+        $user = User::find($params = array('name'=>'michael'), $sort = array(), $fields = array(), $limit = null, $skip = 5);
+        $this->assertSame(0, $user->count());
+    }
+
+    public function testFindWithSort()
+    {
+        $user = User::find($params = array('name'=>'michael'), $sort = array('name'));
+        $this->assertGreaterThan(0, $user->count());
+    }
+
     public function testFindOne()
     {
 
